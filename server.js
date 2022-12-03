08e2199569ef419fe537750f32c89af6c324e16e
brenda70904 <brendaj0214@gmail.com>
2022-12-03 13:06:16 -0800

    Auth0 set up

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+
 const axios = require('axios');
 const User = require('./model/User');
-// const User = require('./model/User');
 
 app.use(cors());
 app.use(express.json());
+const verifyUser = require('./auth');
 
 
 //------connect to MongoDB------//
@@ -23,11 +24,6 @@ db.once('open', function () {
 
 const PORT = process.env.PORT || 3001;
 
-// 1. get data from API
-//set up schema
-// 2. perform CRUD
-// 3. set up Auth0
-
 app.get('/test', (req, res) => {
   res.send('test request received');
 });
@@ -38,15 +34,22 @@ app.get('/displaycocktail', displayCocktail);
 
 //get API data
 async function getCocktails(req, res) {
-  let url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${req.query.i}`;
-  try {
-    let filteredList = await axios.get(url);
-    let drinkList = filteredList.data.drinks.map(obj => new Drinks(obj));
-    let displayDrinks = drinkList.slice(0, 20);
-    res.send(displayDrinks);
-  } catch (e) {
-    res.send(e.error);
-  }
+  verifyUser(req, async (err, user) => {
+    if (err) {
+      console.log(err);
+      res.send('invalid token');
+    } else {
+      let url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${req.query.i}`;
+      try {
+        let filteredList = await axios.get(url);
+        let drinkList = filteredList.data.drinks.map(obj => new Drinks(obj));
+        let displayDrinks = drinkList.slice(0, 20);
+        res.send(displayDrinks);
+      } catch (e) {
+        res.send(e.error);
+      }
+    }
+  });
 }
 
 async function displayCocktail(req, res) {
@@ -82,16 +85,21 @@ class DrinkDetail {//this is from Ninja
   }
 }
 
-
-
 //CRUD
 
 
 // / add new user document/object into mongoDB upon first login
 // app.post('/users', newUser);
 app.post('/users', async (req, res) => {
-  let newUser = await User.create(req.body);
-  res.send(newUser);
+  verifyUser(req, async (err, user) => {
+    if (err) {
+      console.log(err);
+      res.send('invalid token');
+    } else {
+      let newUser = await User.create(req.body);
+      res.send(newUser);
+    }
+  });
 });
 
 // // pull user's info when they login after the first time
@@ -99,48 +107,73 @@ app.post('/users', async (req, res) => {
 app.get('/users/:email', getUserData);
 
 async function getUserData(req, res) {
-  try {
-    let userEmail = req.params.email;
-    let getUserByEmail = await User.findOne({ email: userEmail });
-    res.send(getUserByEmail);
-  } catch (e) {
-    res.send('no data found').status(500);
-  }
+  verifyUser(req, async (err, user) => {
+    if (err) {
+      console.log(err);
+      res.send('invalid token');
+    } else {
+      try {
+        let userEmail = req.params.email;
+        let getUserByEmail = await User.findOne({ email: userEmail });
+        res.send(getUserByEmail);
+      } catch (e) {
+        res.send('no data found').status(500);
+      }
+    }
+  });
 }
-//638aa41cdb5f5552e19bba57
+
+
 
 
 // update user when they add/delete items from bar cart
 // app.put('/users/:email', updateUser);
 app.put('/users/:email', async (req, res) => {
-  try {
-    let updatedData = await User.findOneAndUpdate({email:req.params.email}, {$push:req.body});
-    // getUserData(req.params.email)
-    res.send(updatedData);
-  } catch (e) {
-    res.send(e.message).status(500);
-  }
+  verifyUser(req, async (err, user) => {
+    if (err) {
+      console.log(err);
+      res.send('invalid token');
+    } else {
+      try {
+        let updatedData = await User.findOneAndUpdate({ email: req.params.email }, { $push: req.body });
+        // getUserData(req.params.email)
+        res.send(updatedData);
+      } catch (e) {
+        res.send(e.message).status(500);
+      }
+    }
+  });
 });
 
 app.patch('/users/:email', async (req, res) => {
-  try {
-    let updatedData = await User.findOneAndUpdate({email:req.params.email}, {$pull:req.body});
-    // getUserData(req.params.email)
-    res.send(updatedData);
-  } catch (e) {
-    res.send(e.message).status(500);
-  }
+  verifyUser(req, async (err, user) => {
+    if (err) {
+      console.log(err);
+      res.send('invalid token');
+    } else {
+      try {
+        let updatedData = await User.findOneAndUpdate({ email: req.params.email }, { $pull: req.body });
+        // getUserData(req.params.email)
+        res.send(updatedData);
+      } catch (e) {
+        res.send(e.message).status(500);
+      }
+    }
+  });
 });
 // // if necessary, delete user
 // // app.delete('/users/:email', deleteUser);
 app.delete('/users/:email', async (req, res) => {
-  let deleted = await User.findOneAndDelete(req.params.email);
-  res.send('deleted' + deleted);
+  verifyUser(req, async (err, user) => {
+    if (err) {
+      console.log(err);
+      res.send('invalid token');
+    } else {
+      let deleted = await User.findOneAndDelete(req.params.email);
+      res.send('deleted' + deleted);
+    }
+  });
 });
-
-
-
-
 
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
